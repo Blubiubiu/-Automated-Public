75d7c88a2efe4f32003b37396d6b00097bf385d6
Bali <lisiyuan@tengyue360.com>
2019-08-05 17:28:44 +0800

    v1.0.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <button @click="getData">请求数据</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  methods: {
+    getData() {
+      axios({
+        method: "get",
+        url: "/"
+      });
+    }
+  }
+};
+</script>
