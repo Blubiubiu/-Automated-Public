b8fdf7f8cd778536907953fdd7a88d1bde219f75
panjinsheng <panjinsheng@tengyue360.com>
2019-08-19 11:01:17 +0800

    测试print

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,21 @@
     <button @click="getData">请求数据</button>
     <button @click="getUser">获取用户信息</button>
     111
+    <button @click="print">打印测试</button>
+    <section ref="print">
+      <h1 class="h1">红色粗体</h1>
+      <h2 class="h2">左右40px</h2>
+      打印内容
+      <div class="no-print">不要打印我</div>
+    </section>
   </div>
 </template>
 
 <script>
-import axios from "@/http/request";
 
+import axios from "@/http/request";
+import Print from '../print'
+Vue.use(Print) // 注册
 export default {
   name: "HelloWorld",
   data() {
@@ -20,6 +29,17 @@ export default {
         url: "/test"
       });
     },
+    print(){
+      this.$print(this.$refs.print)
+    },
+    // printHtmlCss() {
+    //   printJS({
+    //     printable: 'test',
+    //     type: 'html',
+    //     css: 'test.css',
+    //     scanStyles: false
+    //   })
+    // },
     getUser() {
       let userInfo = {
         id: 1
@@ -38,3 +58,12 @@ export default {
   }
 };
 </script>
+<style>
+  .h1{
+    background: blue;
+
+  }
+  .h2{
+    margin-left: 40px;
+  }
+</style>
